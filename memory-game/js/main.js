3fe0995d6845b5f836c4ae0347b60ab4caf0aa46
phillip g <phillip g>
2017-03-08 06:19:05 +1100

    Create game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,27 +1,16 @@
-var cOne = "queen";
-var cTwo = "queen";
-var cThree = "king";
-var cFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
 
-console.log("User flipped " + cOne);
+console.log("User flipped " + cardTwo);
+console.log("User flipped " + cardOne);
 
-var max = 5;
-var result;
-
-for (var i = 1; i <= max; i++) {
-    var f = i % 3 == 0;
-    var b = i % 5 == 0;
-    console.log(f ? b ? "FizzBuzz" : "Fizz" : b ? "Buzz" : i);
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You found a match!");
+    }
+    alert("Sorry, try again");
 }
-
-var cups = 0;
-
-while (cups < 33) {
-    console.log("b");
-    cups = cups + 1;
-}
-
-// If x is evenly divisible by both 3 and 5 (for example, 15 or 30), set result to "fizzbuzz".
-// Otherwise if x is evenly divisible by 3 (for example, 3, 6, or 9), set result to "fizz".
-// Otherwise if x is evenly divisible by 5 (for example, 5 or 10), set result to "buzz".
-// If x is not evenly divisible by either 3 or 5 (for example, 7), set result to x.
